db90eaf51b62a2e77b94aa52a0b55711276be6ac
ranjithvunnam <reddy.rkranjith@gmail.com>
2017-09-12 21:31:37 +0530

    UI Issues and few bugs fixed

---
+++ source/wisp-entities/src/main/java/com/nunc/wisp/entities/MainSliderResponseBean.java
@@ -0,0 +1,24 @@
+package com.nunc.wisp.entities;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.nunc.wisp.entities.MainSliderEntity;
+
+public class MainSliderResponseBean implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3258698018689131615L;
+	
+	private List<MainSliderEntity> mainSlider;
+
+	public List<MainSliderEntity> getMainSlider() {
+		return mainSlider;
+	}
+
+	public void setMainSlider(List<MainSliderEntity> mainSlider) {
+		this.mainSlider = mainSlider;
+	}
+}
